ab5044a7f5a5d74f4cd00e092289623408897055
chaips <ar0439708@gmail.com>
2019-10-28 13:11:41 -0600

    Mapas

--- MyApplication/app/src/main/java/com/example/myapplication/Fragment/LlegadaFragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Fragment/LlegadaFragment.java
@@ -4,36 +4,53 @@ package com.example.myapplication.Fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.myapplication.R;
+import com.example.myapplication.TabAdapter;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.tabs.TabLayout;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LlegadaFragment extends Fragment {
-    MapView map;
+public class LlegadaFragment extends Fragment implements OnMapReadyCallback {
+    private GoogleMap map;
+    private TabAdapter adapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
 
     public LlegadaFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-       View rootView = inflater.inflate(R.layout.fragment_llegada, container, false);
-       mapa(rootView);
-       return rootView;
-    }
-    public void mapa(View v){
-        map=v.findViewById(R.id.mapView);
+        View rootView = inflater.inflate(R.layout.fragment_llegada, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        return rootView;
     }
 
+    public void onMapReady(GoogleMap googleMap){
+        map=googleMap;
+        LatLng sydney = new LatLng(-34, 151);
+        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.HorizontalScrollView;
+import android.view.View;
 
 import com.example.myapplication.Fragment.ContactosFragment;
 import com.example.myapplication.Fragment.InformacionFragment;
@@ -20,13 +23,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        viewPager = findViewById(R.id.viewpager);
+        /*viewPager = findViewById(R.id.viewpager);
         tabLayout = findViewById(R.id.tabs);
         adapter = new TabAdapter(getSupportFragmentManager());
         adapter.addFragment(new LlegadaFragment(), "Llegada");
         adapter.addFragment(new ContactosFragment(), "Contactos");
         adapter.addFragment(new InformacionFragment(), "Detalles");
         viewPager.setAdapter(adapter);
-        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);*/
     }
+    public void Lleg(View v){
+        LlegadaFragment vista1Fragment = new LlegadaFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container, vista1Fragment);
+        fragmentTransaction.commit();
+   }
 }
