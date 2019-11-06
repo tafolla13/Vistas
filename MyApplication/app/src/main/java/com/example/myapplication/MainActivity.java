b14f31da098746d497c280c6cc8fadcfc905bba5
chaips <ar0439708@gmail.com>
2019-11-06 12:32:42 -0600

    implementacion de los eventos

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,23 +2,20 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
-import com.example.myapplication.Fragment.MainFragment;
-import com.google.android.material.tabs.TabLayout;
+import com.example.myapplication.gastosembarque.ApartadoEventosFragment;
+import com.example.myapplication.gastosembarque.BlankFragment;
+import com.example.myapplication.gastosembarque.Gastos;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        showSelectedFragment(new MainFragment());
+        showSelectedFragment(new BlankFragment());
     }
     private void showSelectedFragment(Fragment fragment) {
         getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment)
